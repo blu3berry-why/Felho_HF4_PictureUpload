0e13c719acdddacce7b1d7d7abe73a2f326723dc
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2024-05-04 22:43:48 +0200

    wsgi.py update

--- Felho_HF4_PictureUpload/wsgi.py
+++ Felho_HF4_PictureUpload/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Felho_HF4_PictureUpload.settings')
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+# Check for the WEBSITE_HOSTNAME environment variable to see if we are running in Azure Ap Service
+# If so, then load the settings from production.py
+settings_module = 'azureproject.production' if 'WEBSITE_HOSTNAME' in os.environ else 'azureproject.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
