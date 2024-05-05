36c3e3e71e5d71f26ce63677597fda7f244ad758
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2024-05-05 22:48:38 +0200

    requirements.txt

--- manage.py
+++ manage.py
@@ -4,32 +4,9 @@ import os
 import sys
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Felho_HF4_PictureUpload.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
-
-
 # def main():
 #     """Run administrative tasks."""
-#     # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running on Azure App Service
-#
-#     # When running on Azure App Service you should use the production settings.
-#     settings_module = "Felho_HF4_PictureUpload.production" if 'WEBSITE_HOSTNAME' in os.environ else 'Felho_HF4_PictureUpload.settings'
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-#
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Felho_HF4_PictureUpload.settings')
 #     try:
 #         from django.core.management import execute_from_command_line
 #     except ImportError as exc:
@@ -39,6 +16,25 @@ if __name__ == '__main__':
 #             "forget to activate a virtual environment?"
 #         ) from exc
 #     execute_from_command_line(sys.argv)
+
+
+def main():
+     """Run administrative tasks."""
+     # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running on Azure App Service
+
+     # When running on Azure App Service you should use the production settings.
+     settings_module = "Felho_HF4_PictureUpload.production" if 'WEBSITE_HOSTNAME' in os.environ else 'Felho_HF4_PictureUpload.settings'
+     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
+     try:
+         from django.core.management import execute_from_command_line
+     except ImportError as exc:
+         raise ImportError(
+             "Couldn't import Django. Are you sure it's installed and "
+             "available on your PYTHONPATH environment variable? Did you "
+             "forget to activate a virtual environment?"
+         ) from exc
+     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
